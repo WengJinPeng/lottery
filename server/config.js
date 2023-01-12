517bc980411971ce6823937a4457bfe636a41c84
Weng JinPeng <Weng_Jin_Peng@outlook.com>
2023-01-12 21:13:09 +0800

    修改配置

--- server/config.js
+++ server/config.js
@@ -15,51 +15,51 @@ const prizes = [
   },
   {
     type: 1,
-    count: 2,
-    text: "特等奖",
-    title: "神秘大礼",
+    count: 1,
+    text: "一等奖",
+    title: "XiaoMi 12Pro",
     img: "../img/secrit.jpg"
   },
   {
     type: 2,
-    count: 5,
-    text: "一等奖",
-    title: "Mac Pro",
+    count: 1,
+    text: "二等奖",
+    title: "Tissot Watch（天梭手表）",
     img: "../img/mbp.jpg"
   },
   {
     type: 3,
-    count: 6,
-    text: "二等奖",
-    title: "华为 Mate30",
+    count: 3,
+    text: "三等奖",
+    title: "28 inch Luggage",
     img: "../img/huawei.png"
   },
   {
     type: 4,
-    count: 7,
-    text: "三等奖",
-    title: "Ipad Mini5",
+    count: 10,
+    text: "入围奖",
+    title: "Big Duffel Bag",
     img: "../img/ipad.jpg"
   },
   {
     type: 5,
-    count: 8,
-    text: "四等奖",
-    title: "大疆无人机",
+    count: 10,
+    text: "入围奖",
+    title: "Xiaomi Powerbank",
     img: "../img/spark.jpg"
   },
   {
     type: 6,
-    count: 8,
-    text: "五等奖",
-    title: "Kindle",
+    count: 10,
+    text: "入围奖",
+    title: "MINI Electric Shaver",
     img: "../img/kindle.jpg"
   },
   {
     type: 7,
-    count: 11,
-    text: "六等奖",
-    title: "漫步者蓝牙耳机",
+    count: 10,
+    text: "入围奖",
+    title: "SONY Headset",
     img: "../img/edifier.jpg"
   }
 ];
@@ -67,12 +67,12 @@ const prizes = [
 /**
  * 一次抽取的奖品个数与prizes对应
  */
-const EACH_COUNT = [1, 1, 5, 6, 7, 8, 9, 10];
+const EACH_COUNT = [1, 1, 1, 3, 10, 10, 10, 10];
 
 /**
  * 卡片公司名称标识
  */
-const COMPANY = "MoShang";
+const COMPANY = "GOLDEN SANDS CONSTRUCTION & ENGINEERING PTE LTD";
 
 module.exports = {
   prizes,
